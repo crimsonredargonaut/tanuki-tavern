c1d501ba096611048491cbcd73b56a535398d913
crimsonredargonaut <soulxxx014@gmail.com>
2022-12-02 16:41:07 +0800

    moved custom js to main.js

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.dropdown-trigger').dropdown();
+    $('select').formSelect();
+    $('.parallax').parallax();
+    $('.carousel').carousel({
+        numVisible: 3,
+        padding: 25,
+        noWrap:true,
+        fullWidth: true
+    });
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+});
